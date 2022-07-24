77bad510ad278fbc6baa96b2c4d6eac71eea304b
TheThirdEye-DC <thethirdeyesen@gmail.com>
2022-07-24 12:40:20 +0530

    Update zphisher.sh

--- zphisher.sh
+++ zphisher.sh
@@ -153,27 +153,19 @@ kill_pid() {
 ## Banner
 banner() {
 	cat <<- EOF
-		${ORANGE}
-		${ORANGE} ______      _     _     _               
-		${ORANGE}|___  /     | |   (_)   | |              
-		${ORANGE}   / / _ __ | |__  _ ___| |__   ___ _ __ 
-		${ORANGE}  / / | '_ \| '_ \| / __| '_ \ / _ \ '__|
-		${ORANGE} / /__| |_) | | | | \__ \ | | |  __/ |   
-		${ORANGE}/_____| .__/|_| |_|_|___/_| |_|\___|_|   
-		${ORANGE}      | |                                
-		${ORANGE}      |_|                ${RED}Version : 2.2
 
-		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by htr-tech (tahmid.rayat)${WHITE}
+		${ORANGE}  ꧁༒♛ XPHISHER - TheThirdEye ♛༒꧂
+		${ORANGE} 
+
+		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by Darkweb Developers (instander.in)${WHITE}
 	EOF
 }
 
 ## Small Banner
 banner_small() {
 	cat <<- EOF
-		${BLUE}
-		${BLUE}  ░▀▀█░█▀█░█░█░▀█▀░█▀▀░█░█░█▀▀░█▀▄
-		${BLUE}  ░▄▀░░█▀▀░█▀█░░█░░▀▀█░█▀█░█▀▀░█▀▄
-		${BLUE}  ░▀▀▀░▀░░░▀░▀░▀▀▀░▀▀▀░▀░▀░▀▀▀░▀░▀${WHITE} 2.2
+		${ORANGE}  ꧁༒♛ XPHISHER - TheThirdEye ♛༒꧂
+		${ORANGE} 
 	EOF
 }
 
